151d46f0f952a4c80fcdc55063d363d29b818874
AlexRak2 <alejandrorak2@gmail.com>
2024-02-28 19:54:49 -0500

    Added Missing script

---
+++ CopperMatchmaking.Example.Server/RankIds.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CopperMatchmaking.Example.Server;
+
+public enum RankIds : byte
+{
+    Unranked = 0,
+    Bronze = 1,
+    Silver = 2,
+    Gold = 3,
+    Platinum = 4,
+    Diamond = 5,
+    Master = 6,
+    Chaos = 7
+}
